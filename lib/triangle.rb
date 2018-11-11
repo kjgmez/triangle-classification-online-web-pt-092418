31784b259aea6dcfdb908f793b4e5ea92d41952f
Kevin J Gomez <kjgmez1@gmail.com>
2018-11-11 21:45:58 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,16 +6,17 @@ class Triangle
     @side3=three
   end
   def kind
-    if side1.length == 0 && side3.length == 0 && side3.length == 0
-      raise TriangleError
     elsif side1 == side2 && side2 == side3
       :equilateral
     elsif side1 != side3 && side2 == side3 || side1 == side3 && side2 != side3 || side1 == side2 && side2 != side3
       :isosceles
     elsif side1 != side2 && side2 != side3
       :scalene
+    elsif side1 == 0 && side3 == 0 && side3 == 0
+        raise TriangleError
     end
   end
+
   class TriangleError < StandardError
     # def message
     #   "Enter the values of a valid triangel"
