312e55474cf6484642be87297697bd1e238e6271
Kevin J Gomez <kjgmez1@gmail.com>
2018-11-11 21:35:45 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
       :isosceles
     elsif side1 != side2 && side2 != side3
       :scalene
-    elsif 
+    elsif side1.length < 0 && side3.length < 0 && side3.length < 0
       begin
         raise TriangleError
       rescue TriangleError => error
